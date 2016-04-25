41cbbb2d089f6608bdbf8ead5315fd4e294502a3
Mark Lieberman <marklieberman@Marks-MacBook-Pro-2.local>
2016-04-25 00:44:43 -0400

    fixed js logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,10 @@
-var targetNumber = Math.floor((Math.random() * 120) + 19);
-console.log(targetNumber)
+var minNumber = 19;
+var maxNumber = 120
+
+randomNumberFromRange(minNumber, maxNumber);
+
+function randomNumberFromRange(min,max)
+{
+    console.log(Math.floor(Math.random()*(max-min+1)+min));
+}
+//console.log(randomNumberFromRange)
