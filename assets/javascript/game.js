e0438fba0ef44975338a8581202e0ff4ff4ccaf9
Mark Lieberman <marklieberman@Marks-MacBook-Pro-2.local>
2016-04-25 01:35:17 -0400

    Removed possible undeeded line of code from js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 var minNumber = 19;
-var maxNumber = 120
-var crystalValue = [1,2,3,4,5,6,7,8,9,10,11,12]
+var maxNumber = 120;
+var crystalValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-randomNumberFromRange(minNumber, maxNumber);
+//randomNumberFromRange(minNumber, maxNumber);
 
-function randomNumberFromRange(min,max)
-{
+function randomNumberFromRange(min, max){
+	
     document.getElementById("target").innerHTML = (Math.floor(Math.random()*(max-min+1)+min));
 }
 $( "#crystal1" ).click(function() {
