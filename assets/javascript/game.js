55a3b5a6e02eb737f32d29ee9c27878751f91f0c
Mark Lieberman <marklieberman@Marks-MacBook-Pro-2.local>
2016-04-25 01:26:35 -0400

    Attempting to get on clicks to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 var minNumber = 19;
 var maxNumber = 120
+var crystalValue = [1,2,3,4,5,6,7,8,9,10,11,12]
 
 randomNumberFromRange(minNumber, maxNumber);
 
@@ -7,3 +8,6 @@ function randomNumberFromRange(min,max)
 {
     document.getElementById("target").innerHTML = (Math.floor(Math.random()*(max-min+1)+min));
 }
+$( "#crystal1" ).click(function() {
+  alert( "Work damnit." );
+});
