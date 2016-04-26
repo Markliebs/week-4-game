2edf9b67b2db8c44ab1be9e43685390c80ae779d
Mark Lieberman <marklieberman@Marks-MacBook-Pro-2.local>
2016-04-26 19:29:00 -0400

    starting to add game functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,34 @@
 var minNumber = 19;
 var maxNumber = 120;
 var crystalValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
+var crsValue1 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
+console.log(crsValue1)
+var crsValue2 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
+console.log(crsValue2)
+var crsValue3 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
+console.log(crsValue3)
+var crsValue4 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
+console.log(crsValue4);
+var yourNumber = 0
+console.log(yourNumber);
+$('#player').html('<h6>' + yourNumber + '</h6>');
 randomNumberFromRange(minNumber, maxNumber);
 
-function randomNumberFromRange(min, max){
-	
-    document.getElementById("target").innerHTML = (Math.floor(Math.random()*(max-min+1)+min));
+function randomNumberFromRange(min, max) {
+
+	document.getElementById("target").innerHTML = (Math.floor(Math.random() * (max - min + 1) + min));
 }
-$( ".crystal1" ).click(function() {
-  alert( "I think unicorns are kick ass!" );
+
+$(".crystal1").click(function () {
+$('#player').html('<h6>' + crsValue1 + '</h6>');
+	//  alert( "I think unicorns are kick ass!" );
 });
-$( ".crystal2" ).click(function() {
-  alert( "I think unicorns are kick ass!" );
+$(".crystal2").click(function () {
+	alert("I think unicorns are kick ass!");
 });
-$( ".crystal3" ).click(function() {
-  alert( "I think unicorns are kick ass!" );
+$(".crystal3").click(function () {
+	alert("I think unicorns are kick ass!");
 });
-$( ".crystal4" ).click(function() {
-  alert( "I think unicorns are kick ass!" );
+$(".crystal4").click(function () {
+	alert("I think unicorns are kick ass!");
 });
