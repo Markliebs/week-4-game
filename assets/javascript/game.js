0529a6f59a6de8ef9341e6c2b32a94fbfcf8f308
MarkLiebs <marklieberman@Marks-MacBook-Pro-2.local>
2016-04-26 21:48:17 -0400

    Added values to each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,17 @@
 var minNumber = 19;
 var maxNumber = 120;
 var crystalValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var crsValue1 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
-console.log(crsValue1)
-var crsValue2 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
-console.log(crsValue2)
-var crsValue3 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
-console.log(crsValue3)
-var crsValue4 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
-console.log(crsValue4);
-var yourNumber = 0
-var yourWins = 0
-var yourLosses = 0
+var cryValue1 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
+console.log(cryValue1);
+var cryValue2 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
+console.log(cryValue2);
+var cryValue3 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
+console.log(cryValue3);
+var cryValue4 = crystalValue[Math.floor(Math.random() * crystalValue.length)];
+console.log(cryValue4);
+var yourNumber = 0;
+var yourWins = 0;
+var yourLosses = 0;
 
 $('#player').html('<h6>' + yourNumber + '</h6>');
 $('#wins').html('<h6>' + yourWins + '</h6>');
@@ -25,16 +25,19 @@ function randomNumberFromRange(min, max) {
 }
 
 $(".crystal1").click(function () {
-	yourNumber += crsValue1
-$('#player').html('<h6>' + crsValue1 + '</h6>');
-	//  alert( "I think unicorns are kick ass!" );
+	yourNumber += cryValue1;
+$('#player').html('<h6>' + cryValue1 + '</h6>');
+	
 });
 $(".crystal2").click(function () {
-	alert("I think unicorns are kick ass!");
+	$('#player').html('<h6>' + cryValue2 + '</h6>');
+
 });
 $(".crystal3").click(function () {
-	alert("I think unicorns are kick ass!");
+	$('#player').html('<h6>' + cryValue3 + '</h6>');
+
 });
 $(".crystal4").click(function () {
-	alert("I think unicorns are kick ass!");
+	$('#player').html('<h6>' + cryValue4 + '</h6>');
+
 });
